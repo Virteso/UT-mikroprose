55c687d7b7aefc07b9fad039b1181dd896974d18
Virteso <rasmus.lille0@gmail.com>
2026-06-26 06:19:21 +0300

    Reduce 7seg bleeding

--- 7seg.c
+++ 7seg.c
@@ -3,67 +3,68 @@
 #include "7seg.h"
 
 const uint8_t segment_map[] = {
-    0b1111110,
-    0b0110000,
-    0b1101101,
-    0b1111001,
-    0b0110011,
-    0b1011011,
-    0b1011111,
-    0b1110000,
-    0b1111111,
-    0b1111011,
-    0b0000000
+	0b1111110,
+	0b0110000,
+	0b1101101,
+	0b1111001,
+	0b0110011,
+	0b1011011,
+	0b1011111,
+	0b1110000,
+	0b1111111,
+	0b1111011,
+	0	// Clear
 };
 volatile uint8_t SEG_digits[2] = {10, 10};
 volatile uint8_t current_digit = 0;
 
 void shiftByteOut(uint8_t data) {
-    PORTE &= ~(1 << PE4);
-    // Pulse SHCP
-    PORTE |= (1 << PE3);
-    PORTE &= ~(1 << PE3);
+	PORTE &= ~(1 << PE4);
+	// Pulse SHCP
+	PORTE |= (1 << PE3);
+	PORTE &= ~(1 << PE3);
 
-    for (int i = 0; i < 7; i++, data >>= 1) {
-        if (data & 1) {
-            PORTE |= (1 << PE4);
-        } else {
-            PORTE &= ~(1 << PE4);
-        }
+	for (int i = 0; i < 7; i++, data >>= 1) {
+		if (data & 1) {
+			PORTE |= (1 << PE4);
+		} else {
+			PORTE &= ~(1 << PE4);
+		}
 
-        // Pulse SHCP
-        PORTE |= (1 << PE3);
-        PORTE &= ~(1 << PE3);
-    }
-    // Pulse STCP
-    PORTB |= (1 << PB7);
-    PORTB &= ~(1 << PB7);
+		// Pulse SHCP
+		PORTE |= (1 << PE3);
+		PORTE &= ~(1 << PE3);
+	}
+	// Pulse STCP
+	PORTB |= (1 << PB7);
+	PORTB &= ~(1 << PB7);
 }
 
 ISR(TIMER1_COMPA_vect) {
-    current_digit ^= 1;
+	shiftByteOut(0);
+	current_digit ^= 1;
 
-    if (current_digit == 0) {
-        shiftByteOut(segment_map[SEG_digits[0]]);
-        PORTD |= (1 << PD4);
-    } else {
-        shiftByteOut(segment_map[SEG_digits[1]]);
-        PORTD &= ~(1 << PD4);
-    }
+	if (current_digit == 0) {
+		PORTD |= (1 << PD4);
+		shiftByteOut(segment_map[SEG_digits[0]]);
+	} else {
+		PORTD &= ~(1 << PD4);
+		shiftByteOut(segment_map[SEG_digits[1]]);
+	}
 }
 
 void SEGinitIO(){
-    DDRE |= (1 << PE3) | (1 << PE4);
-    DDRB |= (1 << PB7);
-    DDRD |= (1 << PD4);
+	DDRE |= (1 << PE3) | (1 << PE4);
+	DDRB |= (1 << PB7);
+	DDRD |= (1 << PD4);
 }
 
 void SEGinitTimer1(){
-    TCNT1 = 0;
-    OCR1A = 2000000 / (8 * 500);
-    TIMSK1 = (1 << OCIE1A);
-    TCCR1A = (0b11 << WGM10);
-    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11);
+	TCNT1 = 0;
+	OCR1A = 2000000 / (8 * 500);
+	TIMSK1 = (1 << OCIE1A);
+	TCCR1A = (0b11 << WGM10);
+	TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11);
 }
 
 void SEGinit(){
