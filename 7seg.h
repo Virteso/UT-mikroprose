263c01c5ac32976108911f7c9af36ff31feb87dd
Virteso <rasmus.lille0@gmail.com>
2026-06-26 04:53:48 +0300

    Le beginnin

---
+++ 7seg.h
@@ -0,0 +1,14 @@
+#ifndef SEVENSEG_H
+#define SEVENSEG_H
+
+#include <stdint.h>
+
+extern const uint8_t segment_map[];
+extern volatile uint8_t SEG_digits[2];
+extern volatile uint8_t current_digit;
+
+void shiftByteOut(uint8_t data);
+void SEGinitTimer(void);
+void SEGinitIO(void);
+
+#endif
